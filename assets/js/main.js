c723e184b4189d1c233b5cc15e110378985d1a36
Elisabete A Gomes <elisaaugusto770@gmail.com>
2024-08-27 21:43:14 -0300

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,54 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+/* Vai devolver uma string (objeto) */
+function convertPokemonToLi(pokemon) {
+    // Obtendo informações de evolução e gênero
+    const gender = pokemon.gender || 'Unknown';
+    const evolutionCount = pokemon.evolutions ? pokemon.evolutions.length : 0;
+
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
+}
+
+/* Esse é o botão da paginação */
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+    });
+}
+
+loadPokemonItens(offset, limit);
+
+/* O que fazer ao clicar no botão */
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithNexPage = offset + limit;
+
+    /* Para limitar o carregamento da página quando chegar ao máximo de 151 pokemons */
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        /* parentElement é o pai do botão que será removido */
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+});
 
